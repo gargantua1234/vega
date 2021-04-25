b55d8ee234c308af108cfb25e2dbe58d2774f3e4
ablech <arek_b1993@poczta.fm>
2021-04-25 19:52:26 +0200

    added navbar skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
 import React from 'react';
+import Navbar from './component/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
 
 const App = () => (
-    <div className="App">
-        <h1>Main website</h1>
-    </div>
+    <>
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact />
+            </Switch>
+        </Router>
+    </>
 );
 
 export default App;
---
+++ src/component/Button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Button.css';
+
+export const Button = () => <div></div>;
