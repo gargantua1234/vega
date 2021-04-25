00727d533e5976434ed7abdc1cd62f541f6fb091
ablech <arek_b1993@poczta.fm>
2021-04-25 22:42:41 +0200

    removed top too big padding in navbar elements

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,31 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Button from './Button';
 import './Navbar.css';
 
-const Navbar = () => {
+function Navbar() {
     const [click, setClick] = useState(false);
     const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    const showBUtton = () => {
-        if (window.innerWidth <= 960) setButton(false);
-        else setButton(true);
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
     };
 
-    window.addEventListener('resize', showBUtton);
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        TRVL <i className="fab fa-typo3"></i>
+                    <Link
+                        to="/"
+                        className="navbar-logo"
+                        onClick={closeMobileMenu}
+                    >
+                        TRVL
+                        <i class="fab fa-typo3" />
                     </Link>
-                    <div className="manu-icon" onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fa fa-bars'} />
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className="nav-item">
@@ -55,7 +67,8 @@ const Navbar = () => {
                                 Products
                             </Link>
                         </li>
-                        <li className="nav-item">
+
+                        <li>
                             <Link
                                 to="/sign-up"
                                 className="nav-links-mobile"
@@ -72,6 +85,6 @@ const Navbar = () => {
             </nav>
         </>
     );
-};
+}
 
 export default Navbar;
