15b470a1c22f8422e7d2d71cbada474cef2582de
ablech <arek_b1993@poczta.fm>
2021-04-25 21:25:57 +0200

    added navbar

--- src/component/Button.js
+++ src/component/Button.js
@@ -3,4 +3,30 @@ import { Link } from 'react-router-dom';
 
 import './Button.css';
 
-export const Button = () => <div></div>;
+const Button = ({ children, buttonStyle, buttonSize, onClick, type }) => {
+    const FIRST_ELEMENT = 0;
+    const STYLES = ['btn--primary', 'btn--outline'];
+    const SIZES = ['btn--medium', 'btn--large'];
+
+    const checkButtonStyle = STYLES.includes(buttonStyle)
+        ? buttonStyle
+        : STYLES[FIRST_ELEMENT];
+
+    const checkButtonSize = SIZES.includes(buttonSize)
+        ? buttonSize
+        : SIZES[FIRST_ELEMENT];
+
+    return (
+        <Link to="/sign-up" className="btn-mobile">
+            <button
+                className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+                onClick={onClick}
+                type={type}
+            >
+                {children}
+            </button>
+        </Link>
+    );
+};
+
+export default Button;
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Button from './Button';
+import './Navbar.css';
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
+    const showBUtton = () => {
+        if (window.innerWidth <= 960) setButton(false);
+        else setButton(true);
+    };
+
+    window.addEventListener('resize', showBUtton);
+
     return (
         <>
-            <nav classNamłe="navbar">
+            <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
                         TRVL <i className="fab fa-typo3"></i>
@@ -55,6 +65,9 @@ const Navbar = () => {
                             </Link>
                         </li>
                     </ul>
+                    {button && (
+                        <Button buttonStyle="btn--outline">SIGN UP</Button>
+                    )}
                 </div>
             </nav>
         </>
