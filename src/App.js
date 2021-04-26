58cf1585dcd5977a5d8b18f82d3349c66310e99a
ablech <arek_b1993@poczta.fm>
2021-04-26 19:34:18 +0200

    added main page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from './component/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './component/pages/Home';
 
 const App = () => (
     <>
         <Router>
             <Navbar />
             <Switch>
-                <Route path="/" exact />
+                <Route path="/" exact component={Home} />
             </Switch>
         </Router>
     </>
