5c3588348d8761541552428e24e3dd1a361bea2d
ablech <arek_b1993@poczta.fm>
2021-04-26 22:43:48 +0200

    added main pages skeleton

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './component/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './component/pages/Home';
+import Services from './component/pages/Services';
+import Products from './component/pages/Products';
+import SignUp from './component/pages/SignUp';
 
 const App = () => (
     <>
@@ -10,6 +13,9 @@ const App = () => (
             <Navbar />
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/services" component={Services} />
+                <Route path="/products" component={Products} />
+                <Route path="/sign-up" component={SignUp} />
             </Switch>
         </Router>
     </>
--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { HeroSection } from '../HeroSection';
 import '../../App.css';
+import Cards from '../Cards';
 
-const Home = () => {
-    return (
-        <>
-            <HeroSection />
-        </>
-    );
-};
+const Home = () => (
+    <>
+        <HeroSection />
+        <Cards />
+    </>
+);
 
 export default Home;
