225863b103b8e9c73ce072d271c1b60462ecbd1f
ablech <arek_b1993@poczta.fm>
2021-05-04 22:19:56 +0200

    redux simple test

--- src/index.js
+++ src/index.js
@@ -2,4 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from 'redux';
+
+// actions  - describe what action we want to perform in store
+const increment = () => {
+    return {
+        type: 'INCREMENT',
+    };
+};
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT',
+    };
+};
+
+// Reducer - manage how should the store change
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+    }
+};
+
+let store = createStore(counter);
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(increment());
+
+store.dispatch(increment());
+
 ReactDOM.render(<App />, document.getElementById('root'));
