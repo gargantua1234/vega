5e072636ffe84caff6af2e455b1c32e3b445068a
ablech <arek_b1993@poczta.fm>
2021-05-06 19:54:29 +0200

    redux

--- src/index.js
+++ src/index.js
@@ -3,38 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { createStore } from 'redux';
+import allReducers from './reducers';
 
-// actions  - describe what action we want to perform in store
-const increment = () => {
-    return {
-        type: 'INCREMENT',
-    };
-};
-
-const decrement = () => {
-    return {
-        type: 'DECREMENT',
-    };
-};
-
-// Reducer - manage how should the store change
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-    }
-};
-
-let store = createStore(counter);
-
-store.subscribe(() => {
-    console.log(store.getState());
-});
-
-store.dispatch(increment());
-
-store.dispatch(increment());
+const store = createStore(allReducers);
 
 ReactDOM.render(<App />, document.getElementById('root'));
